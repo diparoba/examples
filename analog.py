11a9f6e3872b5f0b8ae4ef60122532c2ed6d697d
Diego Robalino Bastidas <kira_diparoba3@hotmail.com>
2025-11-21 14:33:06 -0500

    Update analog.py

--- analog.py
+++ analog.py
@@ -1,14 +1,14 @@
-from machine import ADC, Pin
+from machine import ADC, Pin 
 import time
 
-ADC_PIN = 34
+POT_PIN = 32 # Pin conectado al potenciómetro
 
-pot = Pin(ADC_PIN) 
-adc = ADC(pot)
-adc.width(ADC.WIDTH_12BIT)
-adc.atten(ADC.ATTN_11DB)
+pot = Pin(POT_PIN) # Configurar el pin como entrada ADC
+adc_pot = ADC(pot) # Crear objeto ADC
+adc_pot.width(ADC.WIDTH_12BIT) # Configurar de 0 a 4095
+adc_pot.atten(ADC.ATTN_11DB) # Rango de voltaje de 0 a 3.6V
 
 while True:
-    valor_adc = adc.read()
-    print(f"Valor ADC : {valor_adc}")
-    time.sleep(0.1)
+    valor_pot = adc_pot.read() # Leer valor del potenciómetro
+    print(f"Valor POT : {valor_pot}") # Imprimir valor leído
+    time.sleep(0.1) # Esperar 100 ms antes de la siguiente lectura
